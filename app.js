93393c458982c122d0cffd24f543dc881d23ac39
Sujaira <susi141@gmail.com>
2019-11-02 18:51:19 -0400

    adding styles

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
+var btn = document.querySelector("#btn");
 var btn1 = document.querySelector("#btn1");
 var btn2 = document.querySelector("#btn2");
 var btn3 = document.querySelector("#btn3");
 var p1Dis = document.querySelector("#p1");
 var p2Dis = document.querySelector("#p2");
 var numInput = document.querySelector("input[type=number]");
-var winningScoreDisplay = document.querySelector("p span");
+var showGame = document.querySelector("#game");
+var namePlsinput = document.querySelectorAll("input[type=text]");
+var namePls = document.querySelectorAll("h2 span");
+var msg = document.querySelector("#msg");
+var bGame = document.querySelector("#bGame");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
 var winningScore = 5;
+
+numInput.value = winningScore;
+showGame.style.display = "none";
+
+btn.addEventListener("click", function() {
+    if (namePlsinput[0].value.length == 0 ||
+        namePlsinput[1].value.length == 0) {
+        msg.textContent = "Please add the names of the players";
+        console.log("empty" + namePlsinput[0].value + " " +
+            namePlsinput[1].value);
+    } else {
+        console.log("go");
+        showGame.style.display = "block";
+        for (var i = 0; i < namePls.length; i++) {
+            namePls[i].textContent = namePlsinput[i].value;
+        }
+        bGame.style.display = "none";
+    }
+
+});
 
 btn1.addEventListener("click", function() {
     if (!gameOver) {
@@ -48,7 +73,6 @@ function reset() {
 };
 
 numInput.addEventListener("change", function() {
-    winningScoreDisplay.textContent = this.value;
     winningScore = Number(this.value);
     reset();
 });
